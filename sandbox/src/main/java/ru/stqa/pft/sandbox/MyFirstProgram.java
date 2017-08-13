10aeb64a25be6af30b5961013b7c33f0ebe6bb21
Helmax27 <Helmax27@users.noreply.github.com>
2017-08-13 23:55:27 +0300

    Использование метода в классе Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,15 +4,9 @@ public class MyFirstProgram {
 
   public static void main(String[] args) {
 
+  Point p = new Point(8.0, 9.0, 7.0, 8.0);
+  System.out.println("Расстояние между двумя точками " + " = " + p.distance());
 
-  Point p1 = new Point(8.0, 9.0);
-  Point p2 = new Point(7.0, 8.0);
-  System.out.println("Расстояние между двумя точками " + " = " + distance(p1, p2));
-
-
-  }
-  public static double distance(Point p1, Point p2) {
-    return Math.sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
   }
 
 }
