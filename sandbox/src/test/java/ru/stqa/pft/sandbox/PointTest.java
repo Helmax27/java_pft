da49ed22ba483f4e64ab19a260c2209261dc65dd
Helmax27 <Helmax27@users.noreply.github.com>
2017-08-14 01:47:17 +0300

    Положительный тест с использованием TestNG

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
@@ -0,0 +1,13 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTest {
+
+  @Test
+  public void testDistance() {
+    Point p = new Point(8.0, 9.0, 7.0, 8.0);
+    Assert.assertEquals(p.distance(), 1.4142135623730951);
+  }
+}
