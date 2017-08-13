61643e6898c14c0c47f49b2decaaf97c42926469
Helmax27 <Helmax27@users.noreply.github.com>
2017-08-13 20:21:39 +0300

    Создан класс Point и функция distance
    Задание №2:
    1. Создан класс Point.
    2. Создана функция distance которая вычисляет расстояние между двумя точкамию

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,27 +3,20 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    hello("world");
-    hello("user");
-    hello("Helen");
 
-    double l = 5;
-    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
 
-    double a = 4;
-    double b = 6;
-    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+  Point p1 = new Point();
+  Point p2 = new Point();
+  p1.x = 8.0;
+  p1.y = 9.0;
+  p2.x = 7.0;
+  p2.y = 8.0;
+  System.out.println("Расстояние между двумя точками " + " = " + distance(p1, p2));
+
+
+  }
+  public static double distance(Point p1, Point p2) {
+    return Math.sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
   }
 
-  public static void hello(String somebody) {
-    System.out.println("Hello, " + somebody + "!");
-  }
-
-  public static double area(double len) {
-    return len * len;
-  }
-
-  public static double area(double a, double b) {
-    return a * b;
-  }
 }
