f280b0722e7983918cb2f38dedbf7c247abcd7d3
Helmax27 <Helmax27@users.noreply.github.com>
2017-08-14 11:50:09 +0300

    Вычисление растояния исправленное с использованием p1.distance(p2)

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,10 +2,11 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 
-  public static void main(String[] args) {
+  public static void main(String args[]) {
 
-  Point p = new Point(8.0, 9.0, 7.0, 8.0);
-  System.out.println("Расстояние между двумя точками " + " = " + p.distance());
+  Point p1 = new Point(8.0, 9.0);
+  Point p2 = new Point(7.0, 8.0);
+  System.out.println("Расстояние между двумя точками " + " = " + p1.distance(p2));
 
   }
 
