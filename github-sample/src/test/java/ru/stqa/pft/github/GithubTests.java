38209fdb76f4f29686b0d45cb0d658fa1a295fda
Helmax27 <Helmax27@users.noreply.github.com>
2017-10-16 23:59:30 +0300

    Реализовано взаимодействие с GitHub

---
+++ github-sample/src/test/java/ru/stqa/pft/github/GithubTests.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.github;
+
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GithubTests {
+
+  @Test
+  public void testCommits() throws IOException {
+    Github github = new RtGithub("eed7951ba39a635d08160d119fa988d9e3164a62");
+    RepoCommits commits = github.repos().get(new Coordinates.Simple("Helmax27", "java_pft")).commits();
+    for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
+      System.out.println(new RepoCommit.Smart(commit).message());
+    }
+  }
+}
